66d07a95f6afb609c201d779f5fc2ba1f9f19d79
Billy Chen <cgi0911@billyvm.cs1cloud.internal>
2015-09-18 21:14:54 -0400

    Added rsvr_sample in_place mode.

--- test2.py
+++ test2.py
@@ -2,23 +2,24 @@
 
 import time
 import os
-from pyvaropt import KWTable
+import pyvaropt as pv
 
 if __name__ == "__main__":
     tot_size = 0
-    fns = sorted(os.listdir("/home/cgi0911/sg3000/Waikato_V_traces/hourly_flowbin/"))
+    fns = sorted(os.listdir("/home/users/cgi0911/Data/Waikato_5/hourly_flowbin"))
     tables = []
 
     st = time.time()
     for i in range (20):
-        t = KWTable(filetype="flowbin", fn="/home/cgi0911/sg3000/Waikato_V_traces/hourly_flowbin/" + fns[i])
+        # "/home/cgi0911/sg3000/Waikato_V_traces/hourly_flowbin/" for phenom1
+        t = pv.KWTable(filetype="flowbin", fn="/home/users/cgi0911/Data/Waikato_5/hourly_flowbin/" + fns[i])
         tables.append(t)
         tot_size += len(t)
 
     print "Reading time =", (time.time() - st)
     print "Original size =", tot_size
 
-    ret = KWTable()
+    ret = pv.KWTable()
 
     st = time.time()
     for t in tables:
